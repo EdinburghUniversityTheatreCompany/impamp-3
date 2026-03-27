6dbb2a7731309757cd7a92551078cc52e30c8427
Mick Zijdel <mickzijdel@hotmail.com>
2026-03-27 21:52:01 +0000

    fix: drivepicker build

--- src/types/drive-picker.d.ts
+++ src/types/drive-picker.d.ts
@@ -1,19 +1,22 @@
-/// <reference types="react" />
+import "react";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "drive-picker": React.HTMLAttributes<HTMLElement> & {
-      "app-id"?: string;
-      "client-id"?: string;
-      "developer-key"?: string;
-      "oauth-token"?: string;
-      "max-items"?: number;
-    };
-    "drive-picker-docs-view": React.HTMLAttributes<HTMLElement> & {
-      "view-id"?: string;
-      "include-folders"?: string;
-      "mime-types"?: string;
-      "owned-by-me"?: string;
-    };
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "drive-picker": React.HTMLAttributes<HTMLElement> & {
+        "app-id"?: string;
+        "client-id"?: string;
+        "developer-key"?: string;
+        "oauth-token"?: string;
+        "max-items"?: number;
+        ref?: React.Ref<HTMLElement>;
+      };
+      "drive-picker-docs-view": React.HTMLAttributes<HTMLElement> & {
+        "view-id"?: string;
+        "include-folders"?: string;
+        "mime-types"?: string;
+        "owned-by-me"?: string;
+      };
+    }
   }
 }
